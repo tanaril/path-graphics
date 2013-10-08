8a74363bbb9233017acd5fa104f352e11b4afdc3
jelte <jelte@marlon.be>
2013-10-08 21:27:03 +0200

    Deployment

--- deployment/config/deploy.rb
+++ deployment/config/deploy.rb
@@ -1,25 +1,93 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+server "www.path-graphics.com", :app, :web, :db, :primary => true
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+# =============================================================================
+# SCM OPTIONS
+# =============================================================================
+set :scm, :git          # or :git
+set :repository,  "git@github.com:tanaril/path-graphics.git"
+set :branch, 'master'
 
-role :web, "www.path-graphics.com"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+# =============================================================================
+# REQUIRED VARIABLES
+# =============================================================================
+set :current_dir, "release"
+set :application, "PATH GRAPHICS"
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+# =============================================================================
+# SSH OPTIONS
+# =============================================================================
+set :deploy_to, "/home/path-graphics"
+set :user, "path-graphics"
+set :use_sudo, false           # optional
+set :ssh_options, { :forward_agent => true }
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+# =============================================================================
+# RELEASE
+# =============================================================================
+set :keep_releases, 3
+set :deploy_via, :copy
+set :copy_cache, false
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+set :copy_exclude, [
+    ".git",
+    ".gitignore",
+    "deployment",
+    "documentation"
+]
+
+# =============================================================================
+# TASKS
+# =============================================================================
+
+after "deploy:create_symlink", "deploy:cleanup"
+after "deploy:cleanup", "deploy:restart"
+
+def remote_file_exists?(full_path)
+  'true' ==  capture("if [ -e #{full_path} ]; then echo 'true'; fi").strip
+end
+
+namespace :deploy do
+
+    task :update_code, :except => { :no_release => true } do
+      on_rollback { run "rm -rf #{release_path}; true" }
+      strategy.deploy!
+      finalize_update
+    end
+
+    task :finalize_update do
+        transaction do
+            run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
+            run "mkdir -r #{release_path}/cache/ngx_pagespeed_cache"
+        end
+    end
+
+    task :create_symlink, :except => { :no_release => true } do
+        on_rollback do
+          if previous_release
+            #run "rm -f #{current_path}; ln -s #{previous_release} #{current_path}; true"
+          else
+            logger.important "no previous release to rollback to, rollback of symlink skipped"
+          end
+        end
+
+        run "rm -f #{current_path} && ln -s #{latest_release} #{current_path}"
+    end
+
+    task :restart do
+        #run "pkill -QUIT -f 'php-fpm: pool #{php_pool}'"
+    end
+end
+
+def with_user(new_user, &block)
+  old_user = user
+  set :user, new_user
+  close_sessions
+  yield
+  set :user, old_user
+  close_sessions
+end
+
+def close_sessions
+  sessions.values.each { |session| session.close }
+  sessions.clear
+end
