9bb7514882e27895798754fbfe6b25c8899647ef
jelte <jelte@marlon.be>
2013-10-15 00:22:36 +0200

    Navbar on each page, remove auto scroll

--- web/js/site.js
+++ web/js/site.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
                 marginTop = (height - containerHeight)/2;
             } else {
                 // Take into account the navigation when calculating the top margin
-                marginTop = (height - containerHeight)/2;
+                marginTop = (height - containerHeight)/2  - $nav.height();
             }
             // Make sure the content stays within the page
             if ( marginTop < 0 ) {
@@ -45,7 +45,7 @@ $(document).ready(function() {
             if ( $(this).attr('id') == 'homepage' ) {
                 $container.css({'paddig-top': marginTop + $container.css("padding-top")});
             } else {
-                $container.css({'padding-top': marginTop < $nav.height() ? $nav.height() : marginTop});
+                $container.css({'padding-top': marginTop < $nav.height() ? marginTop : marginTop});
             }
             // Set the minimum height of the page.
             $(this).css({"min-height": height});
@@ -81,61 +81,7 @@ $(document).ready(function() {
             });
         }
     });
-    /*$('#nav-helper').on('activate.bs.scrollspy', function () {
-     $this = $(this);
-     $.each($this.find('a'), function(index, a) {
-     $this.removeClass($(a).attr('href').replace('#', ''));
-     });
-     $this.addClass($this.find('.active a').attr('href').replace('#', ''));
-     });*/
 
-    var navVisible = -1;
-    var onScroll = function() {
-
-        // Get the current scroll position
-        var currentScroll = $window.scrollTop();
-
-        if ( currentScroll <= $portfolio.offset().top ) {
-            var pos = (currentScroll-($buttons.offset().top + $buttons.height()))/($homepage.height()-($buttons.offset().top + $buttons.height()));
-            if ( pos < 0.05 ) {
-                $nav.css('opacity', 0);
-                $nav.css('display', 'none');
-            } else {
-                $nav.css('display', 'block');
-                $nav.css('opacity', pos > 1?1:pos);
-            }
-        } else {
-            $nav.css('display', 'block');
-            $nav.css('opacity', 1);
-
-            $.each($pages, function(index, page) {
-                $nav.removeClass($(page).attr('id'));
-            });
-
-            if ( currentScroll < $portfolio.offset().top ) {
-                $homepage.siblings('.active').removeClass('active');
-                $homepage.addClass('active');
-
-                $nav.addClass('homepage');
-            } else if ( currentScroll >= $portfolio.offset().top && currentScroll < $about.offset().top) {
-                $portfolio.siblings('.active').removeClass('active');
-                $portfolio.addClass('active');
-
-                $nav.addClass('portfolio');
-            } else if ( currentScroll >= $about.offset().top && currentScroll < $contact.offset().top ) {
-                $about.siblings('.active').removeClass('active');
-                $about.addClass('active');
-
-                $nav.addClass('about');
-            } else if ( currentScroll >= $contact.offset().top ) {
-
-                $contact.siblings('.active').removeClass('active');
-                $contact.addClass('active');
-
-                $nav.addClass('contact');
-            }
-        }
-    };
 
     $(window).on("scroll",function(event) {
 
@@ -153,146 +99,6 @@ $(document).ready(function() {
             } else {
                 $('a.leftarrow').hide(); $('a.rightarrow').hide();
             }
-        } else {
-            onScroll();
-        }
-    });
-
-
-
-
-    /**
-     * Single scroll to scroll to the next page.
-     */
-    function scroll(scrollDelta) {
-            // Get the current scroll position
-            var currentScroll = $window.scrollTop();
-
-            if ( currentScroll <= 0 && scrollDelta <= 0 ) {
-//                /return false;
-            }
-            if ( currentScroll >= $contact.offset().top && scrollDelta > 0 ) {
-//                return false;
-            }
-
-            // Find the currently active navigation item
-            var $current = $(".page.active");
-            if ( $current.length <= 0 ) {
-                // If a hash tag is set in the url activate that page
-                if ( window.location.hash ) {
-                    $current = $(window.location.hash).addClass('active');
-                } else {
-                    // if nothing is set select the first one.
-                    $current = $('.page').first().addClass('active');
-                }
-            }
-            // Check if there is an active navigation item
-            if ( $current ) {
-                var $target;
-
-                // Top position of the current page
-                var pageTop = $current.offset().top;
-                var currentHeight = $current.height();
-                // Absolute bottom of the page
-                var pageBottom = pageTop + currentHeight;
-
-                // Determine scroll direction
-                if (scrollDelta > 0){
-                    // Get the next page
-                    $target = $current.next('.page');
-                    if ( currentScroll + windowHeight > pageBottom && $target.length > 0 ) {
-
-                        $target.siblings('.active').removeClass('active');
-                        $target.addClass('active');
-
-                        // Scroll
-                        $body.animate({
-                            scrollTop: $target.offset().top
-                        }, {
-                            queue: false,
-                            duration: 1500,
-                            easing: 'linear',
-                            progress: onScroll,
-                            done: function() {
-                                setTimeout(function() {
-                                    // Re-enable user scrolling
-                                    autoScroll = false;
-                                }, 400);
-                            }
-                        });
-                        return true;
-                    }
-                } else {
-                    // Get the previous page
-                    $target = $current.prev('.page');
-                    if ( currentScroll <= pageTop && $target.length > 0) {
-
-                        $target.siblings('.active').removeClass('active');
-                        $target.addClass('active');
-
-                        // Scroll
-                        $body.animate({
-                            scrollTop: $target.offset().top
-                        }, {
-                            queue: false,
-                            duration: 1500,
-                            easing: 'linear',
-                            progress: onScroll,
-                            done: function() {
-                                setTimeout(function() {
-                                    // Re-enable user scrolling
-                                    autoScroll = false;
-                                }, 400);
-                            }
-                        });
-                        return true;
-                    }
-                }
-            }
-    }
-
-    $(this).on('DOMMouseScroll', function (e) {
-        if ( scrollEnabled ) {
-            return true;
-        } else {
-            e.preventDefault();
-            e.delta = null;
-            if (e.originalEvent) {
-                if (e.originalEvent.wheelDelta) e.delta = e.originalEvent.wheelDelta / -40;
-                if (e.originalEvent.deltaY) e.delta = e.originalEvent.deltaY;
-                if (e.originalEvent.detail) e.delta = e.originalEvent.detail;
-            }
-
-            // Check if it's not already scrolling
-            if ( !autoScroll ) {
-                autoScroll = true;
-                if ( !scroll(e.delta) ) {
-                    autoScroll = false;
-                }
-            }
-            return false
-        }
-    });
-    $(this).on('mousewheel', function (e) {
-        if ( scrollEnabled ) {
-            return true;
-        } else {
-            e.preventDefault();
-            e.delta = null;
-            if (e.originalEvent) {
-                if (e.originalEvent.wheelDelta) e.delta = e.originalEvent.wheelDelta / -40;
-                if (e.originalEvent.deltaY) e.delta = e.originalEvent.deltaY;
-                if (e.originalEvent.detail) e.delta = e.originalEvent.detail;
-            }
-
-            // Check if it's not already scrolling
-            if ( !autoScroll ) {
-                autoScroll = true;
-                if ( !scroll(e.delta) ) {
-                    autoScroll = false;
-                }
-            }
-            return false
         }
     });
 
